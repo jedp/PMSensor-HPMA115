57d0d4f1c69657445f0ffae3494eadd632633985
Jed Parsons <jed@jedparsons.com>
2020-09-27 17:51:40 -0700

    Tests for state machine

--- include/fake/FakeStream.h
+++ include/fake/FakeStream.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+const uint8_t BUF_LEN = 32;
+
 class Stream {
  public:
   Stream();
@@ -10,6 +12,14 @@ class Stream {
   int peek();
   int read();
   void readBytes(uint8_t *to, uint8_t len);
+
+  /*Add a byte onto the fake stream. */
+  void add(uint8_t byte);
+
+ private:
+  uint8_t buf[BUF_LEN] = { 0 };
+  uint8_t idx = 0;
+  uint8_t end = 0;
 };
 
 #endif  // INCLUDE_FAKE_FAKESTREAM_H_
